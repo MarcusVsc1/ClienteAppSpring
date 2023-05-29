eaf2a04b336d36b7869605adcb512f07ca3c52c2
Marcus Vasconcelos <marcus.cunha@ntendencia.com.br>
2023-05-29 18:19:33 -0300

    Adicionado log de exceção

--- src/main/java/com/ex/clienter/util/BlobUtils.java
+++ src/main/java/com/ex/clienter/util/BlobUtils.java
@@ -4,12 +4,14 @@ import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.ex.clienter.config.BlobConfig;
 import com.google.gson.Gson;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayInputStream;
 import java.time.LocalDateTime;
 
+@Log4j2
 @Component
 public class BlobUtils {
 
@@ -37,7 +39,7 @@ public class BlobUtils {
             blobClient.upload(dataStream);
 
         } catch (Exception e) {
-            System.out.println("Erro ao salvar blob: " + gson.toJson(obj));
+            log.error("Erro ao salvar blob: " + gson.toJson(obj) + ": "+e.getMessage());
         }
 
     }
